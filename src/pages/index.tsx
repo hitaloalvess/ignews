058a538b72f8ff687c3350571f047f77fb5510d7
Hitalo Alves <hitalo.ralves@outlook.com>
2021-09-08 20:12:29 -0300

    Finalizando estilizacao da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import  Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from './home.module.scss';
 
@@ -10,7 +11,11 @@ export default function Home() {
           </Head>
           <main className={styles.contentContainer}>
             <section className={styles.hero}>
-                <p> 👏 </p>
+                <p> 👏 Hey, welcome </p>
+                <h1>News about <br /> the <span>React</span> world</h1>
+                <p>Get acess to all the publications <br /> <span>for $9,90 month</span></p>
+
+                <SubscribeButton />
             </section>
             <img src="/images/avatar.svg" alt="avatar home" />
           </main>
