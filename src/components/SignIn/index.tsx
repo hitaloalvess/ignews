a763a34b33ad5bcd1edb1dac4649f7a965b57f86
Hitalo Alves <hitalo.ralves@outlook.com>
2021-09-10 20:40:26 -0300

    Fazendo integracao do login social com o github

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,29 +1,33 @@
+import { signIn, signOut, useSession} from 'next-auth/client'
 import {FaGithub} from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-    const isUserLoggedIn = false;
-    
-    return isUserLoggedIn ? 
+    const [session] = useSession()
+    console.log(session)
+    return session ? 
             (
-               <button 
+                <button 
                     type="button"
                     className={styles.signInButton}
+                    onClick={ () => signOut()}
                 >
-                    <FaGithub color="#EBA417" />
-                    Sign in with Github
-               </button>
+                    <FaGithub color="#04D361" />
+                    {session.user.name}
+                    <FiX color="#737380" className={styles.closeIcon} />
+                </button>
             ):
             (
                 <button 
                     type="button"
                     className={styles.signInButton}
+                    onClick={ () => signIn('github')}
                 >
-                    <FaGithub color="#04D361" />
-                    Hitalo Rodrigo Alves
-                    <FiX color="#737380" className={styles.closeIcon} />
+                    <FaGithub color="#EBA417" />
+                    Sign in with Github
                </button>
+                
             )
 }
